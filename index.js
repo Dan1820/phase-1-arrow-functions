67eecbc47b1198ebfc8309e8dc2c1a4ad33f6b24
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-16 00:05:11 +0300

    completed

---
+++ index.js
@@ -0,0 +1,23 @@
+// const twoAdder=x=>x+2;
+// console.log(twoAdder(2))
+
+// const sum=(par1,par2)=>{
+//     console.log(`adding ${par1}`);
+//     console.log(`adding ${par2}`);
+//     return par1+par2;
+// }
+// console.log(sum(1,2))
+
+// const nums =[1,2,3,4];
+// const squares= nums.map(x =>x**2);
+// console.log(squares);
+const divide =()=>{
+    return 2000/100
+
+}
+const square=x=>x**2
+const add=(a,b)=>{
+    return a+b
+}
+add(3,4)
+console.log(divide (2000,100));
